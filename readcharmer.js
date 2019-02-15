d1d0d0a4c8234d2af1d9712ac7c37552f88d06fa
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-15 18:49:20 +0000

    Learning to code an annual salary report by dept, testing

--- readcharmer.js
+++ readcharmer.js
@@ -1,27 +1,41 @@
 const fs = require('fs');
 
-const charmeremployees = "charmeremployees.txt";
+// const charmeremployees = "charmeremployees.txt";
 
-let employees = fs.readFileSync(charmeremployees, 'utf8');
+// let employees = fs.readFileSync(charmeremployees, 'utf8');
 
-console.log(employees);
-
-
+// console.log(employees);
 
 
 
 const charmersalaries = "charmersalaries.txt";
 
-let salaries = fs.readFileSync(charmersalaries, 'utf8');
+let salaries = fs.readFileSync(charmersalaries, 'utf8', function(error, data){
+    if(error) throw error;
 
-console.log(salaries);
+    var array = salaries.split()
+    console.log(array);
+
+  for (var i = 0; i < array.length; i++){
+      console.log(array[i]);
+  }
+
+});
 
 
 
 
 
-const charmerdepartments = "charmerdepartments.txt";
 
-let departments = fs.readFileSync(charmerdepartments, 'utf8');
+// const charmerdepartments = "charmerdepartments.txt";
 
-console.log(departments);
+// let departments = fs.readFileSync(charmerdepartments, 'utf8');
+
+// console.log(departments);
+
+
+// const charmerdepartmentlist = "departmentlist.txt";
+
+// let departmentlist = fs.readFileSync(charmerdepartmentlist, 'utf8');
+
+// console.log(departmentlist);
