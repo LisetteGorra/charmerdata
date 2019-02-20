96fb6af18f0ce36cfbfb22611905416630bbcbc8
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-20 16:10:36 +0000

    Comparing codes

--- readcharmer.js
+++ readcharmer.js
@@ -1,55 +1,15 @@
-const fs = require('fs');
+// var business = "Dept Co.";
+// var departmentId = ["d001", "d002", "d003", "d004", ]; 
+// var departments = ["management", "development", "sales", "marketing" ];
+// var employeeId = [ [100, 102, 103, 104, 500], [200, 201, 202, 203, 204, 205], [300, 301, 302, 303], [400, 401, 402] ];
+// var salaries = [ [85000, 85000, 95000, 105000, 150000], [65000, 65000, 75000, 75000, 85000, 95000], [55000, 65000, 75000, 95000], [55000, 65000, 75000] ];
+// var employeeName = [ ["Ruth", "Sofía", "Elias", "Mathias", "Claude"], ["Kai", "Isaiah", "Amare", "Nadia", "Nolan", "Jason"], ["Alejandro", "Steven", "Brodi", "Mary"], ["Jose", "Nia", "Alicia"] ];
 
-const charmeremployees = "charmeremployees.txt";
-
-let employees = fs.readFileSync(charmeremployees, 'utf8');
-
-console.log(employees);
-
-
-
-// const charmersalaries = "charmersalaries.txt";
-
-// let salaries = fs.readFileSync(charmersalaries, 'utf8');
-
-// console.log(charmersalaries);
-
-
-
+// for (var i = 0; i < departmentId.length; i++) {
+//     console.log(`Department ${departmentId[i]} - ${departments[i]}:`);
+//       for (var j = 0; j < employeeId[i].length; j++) {
+//           console.log(`  ${j+1}. Employee ID: ${employeeId[i][j]}, Name: ${employeeName[i][j]}, Salary: ${salaries[i][j]}`);
+        
+//     }
  
- 
-
-
-
-
-
-
-
-
-
-
-// const charmerdepartments = "charmerdepartments.txt";
-
-// let departments = fs.readFileSync(charmerdepartments, 'utf8');
-
-// console.log(departments);
-
-
-
-
-
-
-
-
-// const charmerdepartmentlist = "departmentlist.txt";
-
-// let departmentlist = fs.readFileSync(charmerdepartmentlist, 'utf8');
-
-// console.log(departmentlist);
-
-
-
-
-//  cat charmersalaries.txt | sed -n '/9999/p'
-
-// cat filename.txt | sed -n '/9999/p'
+// }
