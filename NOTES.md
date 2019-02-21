c01f9558dc13110d309afee9124ab22540959417
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-21 16:04:23 +0000

    Breaking down information to its simplest form, and console logging additional info into array

---
+++ NOTES.md
@@ -0,0 +1,8 @@
+var departments = ["management","training","smartPeople"]
+var departmentID = ["d01","d02","d03"];
+var employeeID = [[101, 102, 103],[201, 202, 203],[1005, 1111, 303]];
+var employeeName = [["Max", "Aurthur", "Riley"], ["Zoe", "Jade", "Norma"], ["Jed","Chris","Dante"]];
+
+employeeID[2][1] = 1003;
+
+line 6 is a way to get employee id number into array while using the console
--- deptsalary01.js
+++ deptsalary01.js
@@ -1,21 +1,56 @@
-var business = "Dept Co.";
-var departmentID = ["d001", "d002", "d003", "d004"];
-var departments = ["management", "development", "sales", "marketing"];
-var employeeID = [[100, 101, 102, 103, 104, 105], [200, 201, 202, 203, 204, 205], [300, 301, 302, 303, 304, 305], [400, 401, 402, 403, 404, 405]];
-var salaries = [[8500, 8500, 8500, 8500, 8500, 8500], [9500, 9500, 9500, 9500, 9500, 9500], [10500, 10500, 10500, 10500, 10500, 10500],[11500, 11500, 11500, 11500, 11500, 11500]];
-var employeeName = [["Ruth", "Shirly", "Kim"], ["Blanch", "Kate", "Barbs"],["Tony", "Tom", "Chris"]]
+var fs = require("fs");
+
+//Step 1 - create all single-d and multi-d arrays AS empty arrays (initially)
+    //Push single string data/elemnts into an array as a sigle element 
+    //Push array data into an array to form multi-d arrays 
 
 
-console.log(`departments ${departmentID[0]} employee name: ${employeeName[0][0]}`);
 
 
-for (var i = 0; i < departmentID.length; i++){
-    console.log(`Department ${departmentID[i]} - ${departments[i]}:`);
-        for(var j =0; j < employeeID[i].length; j++) {
-            console.log(` ${j+1}. Employee ID: ${employeeID[i][j]}, Name: ${employeeID[i][j]}, Salary: ${salaries[i][j]}`);
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var business = "Dept Co.";
+// var departmentID = ["d001", "d002", "d003", "d004"];
+// var departments = ["management", "development", "sales", "marketing"];
+// var employeeID = [[100, 101, 102, 103, 104, 105], [200, 201, 202, 203, 204, 205], [300, 301, 302, 303, 304, 305], [400, 401, 402, 403, 404, 405]];
+// var salaries = [[8500, 8500, 8500, 8500, 8500, 8500], [9500, 9500, 9500, 9500, 9500, 9500], [10500, 10500, 10500, 10500, 10500, 10500],[11500, 11500, 11500, 11500, 11500, 11500]];
+// var employeeName = [["Ruth", "Shirly", "Kim"], ["Blanch", "Kate", "Barbs"],["Tony", "Tom", "Chris"]]
+
+
+// console.log(`departments ${departmentID[0]} employee name: ${employeeName[0][0]}`);
+
+
+// for (var i = 0; i < departmentID.length; i++){
+//     console.log(`Department ${departmentID[i]} - ${departments[i]}:`);
+//         for(var j =0; j < employeeID[i].length; j++) {
+//             console.log(` ${j+1}. Employee ID: ${employeeID[i][j]}, Name: ${employeeID[i][j]}, Salary: ${salaries[i][j]}`);
+//         }
     
-}
+// }
 
 
 // Time is a nested forloop
