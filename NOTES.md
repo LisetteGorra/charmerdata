95e87b780ba1dc37ca7a9bd41648057922ca5b0f
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-02-21 18:55:09 +0000

    Finding the index of to get data into arrays

--- NOTES.md
+++ NOTES.md
@@ -93,3 +93,94 @@ fs.readFile('charmerdepartments.txt', 'utf8', function(error, data){
     console.log(departments);
     
 });
+
+
+full code :
+
+var fs = require("fs");
+
+//Step 1 - create all single-d and multi-d arrays AS empty arrays (initially)
+    //Push single string data/elemnts into an array as a sigle element 
+    //Push array data into an array to form multi-d arrays 
+
+
+//Single dimension arrays
+var departmentID = [];
+var departments = [];
+
+//Multi dimension array
+var employeeID = [[]];
+var salaries =[[]];
+var employeeName = [[]];
+
+// Proccess the file charmerdepartments.txt
+fs.readFile('departmentlist.txt', 'utf8', function(error, data){
+    if (error) throw error;
+    
+    // console logging the original data
+    //  console.log("dirty:");
+    // console.log(data);
+    
+    
+    //Remove the uneeded string from the first line 
+    var cleanDeptData = data.replace(/INSERT INTO `departments` Values/g, "")
+   
+   //convert cleaned data into a temporary array. Each line will be an array element
+   
+   
+ //  console logging the cleaned data
+    // console.log("cleaned:");
+    // console.log(cleanDeptData);
+    
+    var deptDataArray = cleanDeptData.split('\n');
+    //deptDataArray.shift(); to clean anything above line 1 that you dont want 
+    // console.log(deptDataArray);
+
+    //grab the department id and the department name and push them into their respective single -d arrays 
+    for (var i = 0; i < deptDataArray.length; i++) {
+        // departmentID.push(deptDataArray[i].slice(2,6));
+        
+        departmentID.push(deptDataArray[i].slice(2,6));
+        departments.push(deptDataArray[i].slice(9,-3));
+
+    }
+    
+    console.log(departmentID);
+    console.log(departments);
+    
+});
+
+
+
+clean employee data 
+
+fs. readFile('charmerdepartments.txt', 'utf8', function(err, data){
+    if(err) throw err;
+   var cleanEmployeeData = data.replace(/INSERT INTO `departments` Values/g, "");
+   
+        
+        var employeeDataArray = cleanEmployeeData.split('\n');
+
+        for ( var i = 0; i < employeeDataArray.length; i++){
+            if(employeeDataArray[i].slice(29,33) == '9999'){
+                console.log(employeeDataArray[i]);
+                
+                // finding the index of 
+                     console.log(employeeDataArray[i].slice(9, 13)); <<< dept
+                    departmentID.indexOf(employeeDataArray[i].slice(8, 12)); << dept index
+                                        console.log(employeeDataArray[i].slice(2, 6)); << this will give you employee id as well 
+then add 
+ employeeID[departmentID.indexOf(employeeDataArray[i].slice(9, 13))].push(employeeDataArray[i].slice(2, 6)); 
+ 
+ and clean up like so:
+ 
+   for (var i = 0; i < employeeDataArray.length; i++) {
+                if (employeeDataArray[i].slice(29, 33) == '9999') {
+             employeeID[departmentID.indexOf(employeeDataArray[i].slice(9, 13))].push(employeeDataArray[i].slice(2, 6));
+                }
+                
+                (we have refactored the code above)
+                    
+            }
+            
+        }
--- deptsalary01.js
+++ deptsalary01.js
@@ -1,8 +1,8 @@
 var fs = require("fs");
 
 //Step 1 - create all single-d and multi-d arrays AS empty arrays (initially)
-    //Push single string data/elemnts into an array as a sigle element 
-    //Push array data into an array to form multi-d arrays 
+//Push single string data/elemnts into an array as a sigle element 
+//Push array data into an array to form multi-d arrays 
 
 
 //Single dimension arrays
@@ -10,90 +10,88 @@ var departmentID = [];
 var departments = [];
 
 //Multi dimension array
-var employeeID = [[]];
-var salaries =[[]];
-var employeeName = [[]];
+var employeeID = [];
+var salaries = [];
+var employeeName = [];
 
 // Proccess the file charmerdepartments.txt
-fs.readFile('departmentlist.txt', 'utf8', function(error, data){
+fs.readFile('departmentlist.txt', 'utf8', function(error, data) {
     if (error) throw error;
-    
+
     // console logging the original data
     //  console.log("dirty:");
     // console.log(data);
-    
-    
+
+
     //Remove the uneeded string from the first line 
-    var cleanDeptData = data.replace(/INSERT INTO `departments` Values/g, "")
-   
-   //convert cleaned data into a temporary array. Each line will be an array element
-   
-   
- //  console logging the cleaned data
+    var cleanDeptData = data.replace(/INSERT INTO `departments` Values/g, "");
+
+    //convert cleaned data into a temporary array. Each line will be an array element
+    var deptDataArray = cleanDeptData.split('\n');
+
+
+//create multi -d arrays using the length of the number of departments 
+    for (var i =0; i < deptDataArray.length; i++){
+        
+ }
+
+
+    //  console logging the cleaned data
     // console.log("cleaned:");
     // console.log(cleanDeptData);
-    
-    var deptDataArray = cleanDeptData.split('\n');
+
+    // var deptDataArray = cleanDeptData.split('\n');
     //deptDataArray.shift(); to clean anything above line 1 that you dont want 
     // console.log(deptDataArray);
 
     //grab the department id and the department name and push them into their respective single -d arrays 
     for (var i = 0; i < deptDataArray.length; i++) {
-        // departmentID.push(deptDataArray[i].slice(2,6));
-        
-        departmentID.push(deptDataArray[i].slice(2,6));
-        departments.push(deptDataArray[i].slice(9,-3));
+        departmentID.push(deptDataArray[i].slice(2, 6));
+        departments.push(deptDataArray[i].slice(9, -3));
 
     }
-    
-    console.log(departmentID);
-    console.log(departments);
-    
+   
+   
+    //create multi -d arrays using the length of the number of departments 
+     for (var i =0; i < deptDataArray.length; i++){
+       employeeID.push([]);
+       employeeName.push([]);
+       salaries.push([]);
+       
+    }
+    console.log(employeeID);
+
+
 });
 
+// process the file 'charmeremployees.txt'
+//('10002,'d007','1996-08-03','9999-01-01'),
+//mutli-d array = employeeID
+
+fs.readFile('charmerdepartments.txt', 'utf8', function(err, data) {
+    if (err) throw err;
+    var cleanEmployeeData = data.replace(/INSERT INTO `departments` Values/g, "");
 
 
+    var employeeDataArray = cleanEmployeeData.split('\n');
+
+    for (var i = 0; i < employeeDataArray.length; i++) {
+        if (employeeDataArray[i].slice(29, 33) == '9999') {
+            // console.log(employeeDataArray[i]);
+            var cleanEmployeeData = data.replace(/INSERT INTO `departments` Values/g, "");
 
 
+            var employeeDataArray = cleanEmployeeData.split('\n');
 
+            for (var i = 0; i < employeeDataArray.length; i++) {
+                if (employeeDataArray[i].slice(29,33) == '9999') {
+                     employeeID[departmentID.indexOf(employeeDataArray[i].slice(9,13))].push(employeeDataArray[i].slice(2,7));
+                     console.log(employeeID);
+                }
 
+            }
 
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var business = "Dept Co.";
-// var departmentID = ["d001", "d002", "d003", "d004"];
-// var departments = ["management", "development", "sales", "marketing"];
-// var employeeID = [[100, 101, 102, 103, 104, 105], [200, 201, 202, 203, 204, 205], [300, 301, 302, 303, 304, 305], [400, 401, 402, 403, 404, 405]];
-// var salaries = [[8500, 8500, 8500, 8500, 8500, 8500], [9500, 9500, 9500, 9500, 9500, 9500], [10500, 10500, 10500, 10500, 10500, 10500],[11500, 11500, 11500, 11500, 11500, 11500]];
-// var employeeName = [["Ruth", "Shirly", "Kim"], ["Blanch", "Kate", "Barbs"],["Tony", "Tom", "Chris"]]
-
-
-// console.log(`departments ${departmentID[0]} employee name: ${employeeName[0][0]}`);
-
-
-// for (var i = 0; i < departmentID.length; i++){
-//     console.log(`Department ${departmentID[i]} - ${departments[i]}:`);
-//         for(var j =0; j < employeeID[i].length; j++) {
-//             console.log(` ${j+1}. Employee ID: ${employeeID[i][j]}, Name: ${employeeID[i][j]}, Salary: ${salaries[i][j]}`);
-//         }
-    
-// }
-
-
-// Time is a nested forloop
+});
